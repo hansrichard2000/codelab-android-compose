f06979449ff1b5edcaa2f15c23de5357e37c56b9
hansrichard2000 <hansrichard2000@gmail.com>
2023-08-08 19:56:32 +0700

    feat: add new code for migration codelab

--- MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
+++ MigrationCodelab/app/src/main/java/com/google/samples/apps/sunflower/plantdetail/PlantDetailDescription.kt
@@ -19,10 +19,17 @@ package com.google.samples.apps.sunflower.plantdetail
 import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.tooling.preview.Preview
 
 @Composable
 fun PlantDetailDescription() {
     Surface {
         Text("Hello Compose")
     }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PlantDetailDescriptionPreview() {
+    PlantDetailDescription()
 }
